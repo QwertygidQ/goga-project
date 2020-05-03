b715ef2c706ae50ce4869cec071e18c148316dab
syn <isaqtm@gmail.com>
2020-05-03 17:40:54 +0300

    Make events many-to-many to users

--- src/database/many_to_many.py
+++ src/database/many_to_many.py
@@ -7,3 +7,8 @@ course_to_user = Table(
     Column("course_id", Integer, ForeignKey("courses.id")),
     Column("user_id", Integer, ForeignKey("users.id"))
 )
+
+user_to_event = Table('user_to_event', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('event_id', Integer, ForeignKey('events.id'))
+)
--- src/database/user.py
+++ src/database/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, Boolean
 from sqlalchemy.orm import relationship
 
 from . import Base
-from .many_to_many import course_to_user
+from .many_to_many import course_to_user,user_to_event
 
 class User(Base):
     __tablename__ = "users"
@@ -16,7 +16,7 @@ class User(Base):
     can_invite_posters = Column(Boolean, nullable=False, default=False)
     can_invite_students = Column(Boolean, nullable=False, default=False)
 
-    events = relationship('Event', back_populates="recipient")
+    events = relationship('Event', secondary=user_to_event, back_populates="users")
     courses = relationship("Course", secondary=course_to_user, back_populates="users")
 
     def __repr__(self):
