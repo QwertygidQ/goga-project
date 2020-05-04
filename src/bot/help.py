11f069fa902f291834df65400d45cdb6f43e3974
QwertygidQ <Qwertygid@gmail.com>
2020-05-05 00:45:14 +0300

    Edited help.py

--- src/bot/help.py
+++ src/bot/help.py
@@ -7,7 +7,8 @@ def help(update, context):
         "/help -- показывает все команды\n"
         "/start -- регистрирует пользователя в системе\n"
         "/create_group [название группы] -- создает новую группу\n"
-        "/invite [название группы] -- создает новый инвайт в группу"
+        "/invite [название группы] -- создает новый инвайт в группу\n"
+        "/join [инвайт] -- присоединяет пользователя к группе"
     )
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=help_text)
